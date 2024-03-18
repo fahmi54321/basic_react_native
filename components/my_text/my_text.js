fd2f513ed7ea1c545f11f16eae302273af6c25de
fahmi <fahmiabdulaziz44@gmail.com>
2024-03-18 08:20:49 +0700

    component text

---
+++ components/my_text/my_text.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import style from './styles';
+
+const MyText = () => {
+  return <Text style={style.text}>Hello</Text>;
+};
+
+export default MyText;
