92c721437443881ba3c953145a02475f4d0b1af6
fahmi <fahmiabdulaziz44@gmail.com>
2024-03-19 20:40:50 +0700

    implement prop

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import MyText from './components/my_text/my_text';
+import Item from './components/item/item';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <MyText />
+      <Item name={'Table'} price={20} />
+      <Item name={'Chair'} price={100} />
+      <Item name={'Desk'} price={200} />
+      <Item name={'Pencil'} price={20} />
     </SafeAreaView>
   );
 };
